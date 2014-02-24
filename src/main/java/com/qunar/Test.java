419838370f5ad2035c198392d72e76bcd63bd169
chaisencs <chaisencs@gmail.com>
2014-02-24 16:56:44 +0800

    test

---
+++ src/main/java/com/qunar/Test.java
@@ -0,0 +1,12 @@
+package com.qunar;
+
+/**
+ * Date: 2014-02-24 16:56
+ *
+ * @author sen.chai
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is my netty world!");
+    }
+}
